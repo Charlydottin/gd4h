eefd15f2730689ac94b7f3669ad3f5ab52574778
c24b Constance de Quatrebarbes <4barbes@gmail.com>
2022-02-04 14:40:19 +0100

    [ARCHI] add populate db + indexation into pipeline commenting out generate app

--- pipeline.py
+++ pipeline.py
@@ -3,10 +3,16 @@
 import os
 import shutil
 from scripts.init_db import import_rules, import_references
+from scripts.populate_db import import_datasets, import_organizations
 from scripts.generate_api import generate_app
+from scripts.setup_index import create_index
+from scripts.setup_index import index_documents
 import argparse
+
+LANGS = ["en", "fr"]
 parser = argparse.ArgumentParser()
 parser.add_argument("app_name")
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -15,11 +21,19 @@ if __name__ == "__main__":
     print("Initialize DB")
     import_rules()
     import_references()
+    print("Populate db")
+    import_datasets()
+    import_organizations()
+    print("Indexing documents")
+    for lang in LANGS:
+        for model in ["dataset", "organization"]:
+            create_index(model, lang)
+            index_documents(model, lang)
     print(f"Generate new app into {app_name}")
     # # back_app_name = "new_back"
-    back_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), app_name)
-    try:
-        shutil.rmtree(back_dir)
-    except FileNotFoundError:
-        pass
-    generate_app(app_name)
+    # back_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), app_name)
+    # try:
+    #     shutil.rmtree(back_dir)
+    # except FileNotFoundError:
+    #     pass
+    # generate_app(app_name)
